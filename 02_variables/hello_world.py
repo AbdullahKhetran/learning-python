a7404d1e535a49bcd1a36ce9aef73f3cc1991186
Abdullah Khetran <khetranabdullah@gmail.com>
2023-10-29 10:08:49 +0500

    used variables

---
+++ 02_variables/hello_world.py
@@ -0,0 +1,11 @@
+message = "Hello Python world!"
+print(message)
+
+message = "Hello Python Crash Course World!"
+print(message)
+
+message = "Hello python crash course reader!"
+# print(mesage)
+
+mesage = "Hello misspled variable"
+print(mesage)
