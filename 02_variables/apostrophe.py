ee34b5ee4213c00ac8b3e216e3dabb5178135720
Abdullah Khetran <khetranabdullah@gmail.com>
2023-10-30 13:04:20 +0500

    apostrophe

---
+++ 02_variables/apostrophe.py
@@ -0,0 +1,5 @@
+message = "One of python's strengths is its diverse community."
+print(message)
+
+# error here, since single quote inside single quote
+# wrong_message = 'One of python's strengths is its diverse community.'
