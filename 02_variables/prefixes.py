dd5ca9a849824fea0457e377bac9dc0c4e8174ab
Abdullah Khetran <khetranabdullah@gmail.com>
2023-10-30 13:00:17 +0500

    removing prefixes

---
+++ 02_variables/prefixes.py
@@ -0,0 +1,7 @@
+nostarch_url = "https://nostarch.com"
+simple_url = nostarch_url.removeprefix("https://")
+
+print(simple_url)
+
+# original value remains unchanged, methods returns a new value
+print(nostarch_url)
