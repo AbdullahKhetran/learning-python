487a0a31a250c45279a2a88160ba7cc8b725aa12
Abdullah Khetran <khetranabdullah@gmail.com>
2023-10-31 09:54:10 +0500

    integers and floats

---
+++ 02_variables_and_simple_data_types/numbers/integers_and_floats.py
@@ -0,0 +1,14 @@
+num1 = 4
+num2 = 2
+
+
+# when you divide any two numbers even if they are integers you get float
+print(num1 / num2)
+
+# mixing integetrs and float will result in float
+num3 = 6.0
+
+print(num1 + num3)
+print(num1 * num3)
+
+# deaults to float when a float is used in an operation, even if answer is whole number
