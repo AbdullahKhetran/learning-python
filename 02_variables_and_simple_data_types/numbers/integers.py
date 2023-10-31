edb1f9c70d988a3003d2328937e3080db170ba35
Abdullah Khetran <khetranabdullah@gmail.com>
2023-10-31 09:30:55 +0500

    integer operations

---
+++ 02_variables_and_simple_data_types/numbers/integers.py
@@ -0,0 +1,16 @@
+num1 = 4
+num2 = 2
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+
+# two asterick to represent exponents
+
+print(num1 ** num2)
+
+# BODMAS
+print(2 + 3 * 4)
+print((2+3)*4)
+# spacing doesnt matter
