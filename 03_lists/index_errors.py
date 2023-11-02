d1c517b21aefca34c1f9412dbbc3769074f3fd9a
Abdullah Khetran <khetranabdullah@gmail.com>
2023-11-02 09:13:40 +0500

    how to avoid index errors

---
+++ 03_lists/index_errors.py
@@ -0,0 +1,16 @@
+# IndexError: list index out of range
+
+# we start counting from 1, but in python list index starts from 0. So the first item in list is at index 0
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+# this will result in error, because there is no 4th item in list
+# print(motorcycles[3])
+
+# if you want to access last item, just use negative index
+print(motorcycles[-1])
+
+# negative index can cause error when list is empty
+# todo: add type
+cars = []
+# error here
+# print(cars[-1])
