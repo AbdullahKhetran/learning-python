9475d5f6ba1ad69f4041ad69ece5a9b8d9431c43
Abdullah Khetran <khetranabdullah@gmail.com>
2023-11-03 18:42:46 +0500

    styling guide according to PEP 8

---
+++ 04_working_with_lists/styling_code.py
@@ -0,0 +1,28 @@
+# Style Guide
+# Python Enhancement Proposal (PEP)
+# PEP 8 introduced many recommendations on styling code
+# Some of them are
+
+# Indentation
+# for indent should be equal to 4 spaces
+
+# Line Length
+# Code line length at max should be 79 characters
+# Comment line length should be max 72 characters
+
+# Blank lines
+# use a blank line to seperate parts of your program
+
+print("Some of PEP 8 recommendations")
+
+print("\nIndentation")
+print("Indent should be equal to 4 spaces")
+
+print("\nLine Length")
+print("Code: Max 79 characters")
+print("Commetns: Max 72 characters")
+
+print("\nBlank lines")
+print("Use blank lines to seperate group")
+
+# Link to PEP 8 : https://peps.python.org/pep-0008/
