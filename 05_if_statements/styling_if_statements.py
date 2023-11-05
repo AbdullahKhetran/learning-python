7e9f8cd2d6847a6534f8936b6d0de259e413d8d6
Abdullah Khetran <khetranabdullah@gmail.com>
2023-11-05 13:45:54 +0500

    PEP 8 recommendation to style if statement

---
+++ 05_if_statements/styling_if_statements.py
@@ -0,0 +1,16 @@
+# PEP 8 recommendation for styling if statements
+
+# use a single space around comparision operators; == > < >= <=
+
+age = 18
+
+if age == 20:
+    print("hello")
+if age > 20:
+    print("hello")
+if age < 20:
+    print("hello")
+if age >= 20:
+    print("hello")
+if age <= 20:
+    print("hello")
