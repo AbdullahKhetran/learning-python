dff130b4eb4da76665ff5cc67c7729d9ebef5004
Abdullah Khetran <khetranabdullah@gmail.com>
2023-11-10 18:28:34 +0500

    counting using while loop

---
+++ 07_user_input_and_while_loops/02_while_loops.py
@@ -0,0 +1,7 @@
+# while loop runs as long as a certain condition is true
+
+# counting using while loop
+current_number = 1
+while current_number <= 5:
+    print(current_number)
+    current_number += 1
