21798e747c3e3a7dcef78da9e901a04bc2a448e2
Abdullah Khetran <khetranabdullah@gmail.com>
2023-11-11 14:31:30 +0500

    combining with user input

--- 07_user_input_and_while_loops/02_while_loops.py
+++ 07_user_input_and_while_loops/02_while_loops.py
@@ -5,3 +5,15 @@ current_number = 1
 while current_number <= 5:
     print(current_number)
     current_number += 1
+
+
+# letting user choose when to quit
+prompt = "\nTell me something, and I will repeat it back to you:"
+prompt += "\nEnter 'quit' to end the program. "
+
+message = ""
+while message != "quit":
+    message = input(prompt)
+# we dont want the quit value to be printed
+    if message != "quit":
+        print(message)
