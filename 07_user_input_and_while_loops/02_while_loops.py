27a8b6f478a512d0c34c62131a5d488c589f8e46
Abdullah Khetran <khetranabdullah@gmail.com>
2023-11-12 20:44:37 +0500

    flag, break, continue and infinite loops

--- 07_user_input_and_while_loops/02_while_loops.py
+++ 07_user_input_and_while_loops/02_while_loops.py
@@ -7,13 +7,78 @@ while current_number <= 5:
     current_number += 1
 
 
+print("")
 # letting user choose when to quit
 prompt = "\nTell me something, and I will repeat it back to you:"
-prompt += "\nEnter 'quit' to end the program. "
+prompt += "\nEnter 'q' to end the program. "
 
 message = ""
-while message != "quit":
+while message != "q":
     message = input(prompt)
-# we dont want the quit value to be printed
-    if message != "quit":
+    # we dont want the quit value to be printed
+    if message != "q":
         print(message)
+
+
+print("")
+# Using a flag
+# For a program that should run only as long as many conditions are
+# true, you can define one variable that determines whether or not
+# the entire program is active.
+
+prompt = "\nTell me something, and I will repeat it back to you:"
+prompt += "\nEnter 'q' to end the program. "
+
+active = True
+while active:
+    message = input(prompt)
+
+    if message == "q":
+        active = False
+    else:
+        print(message)
+
+
+print("")
+# Using break to exit a loop
+
+prompt = "\nPlease enter the name of city you have visited:"
+prompt += "\n(Enter 'q' when you are finished.) "
+
+while True:
+    city = input(prompt)
+
+    if city == "q":
+        break
+    else:
+        print(F"I'd love to go to {city.title()}!")
+
+# break keyword can be used in any loop
+
+
+print("")
+# Continue keyword
+# continue will return to the beginning of the loop
+
+# printing only odd numbers
+current_number = 0
+while current_number < 10:
+    current_number += 1
+    if current_number % 2 == 0:
+        # go back to start of loop
+        continue
+
+    print(current_number)
+
+
+print("")
+# Avoiding infinite loops
+# Make sure at least one part of the program can make
+# the loop’s condition False or cause it to reach a break statement.
+
+x = 1
+# the condition
+while x < 5:
+    print(x)
+    # if next line is omitted it will be infinite loop
+    x += 1
