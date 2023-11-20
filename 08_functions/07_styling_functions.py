4f214f91c39c5f650fcbddf87a2a3f45b3047144
Abdullah Khetran <khetranabdullah@gmail.com>
2023-11-20 17:46:01 +0500

    styling functions according to PEP 8

---
+++ 08_functions/07_styling_functions.py
@@ -0,0 +1,23 @@
+# descriptive name
+# lowercase letters
+# underscores
+
+# a comment
+#   explain what the function does
+#   immediately after function defination
+#   should use docstring format
+
+# for default value of parameter no space around = sign
+#   def function_name(parameter_0, parameter_1="default value")
+
+# limit code to 79 characters
+#   if greater than that then next line and press TAB twice
+# def function_name(
+#       parameter_0, parameter_1, parameter_2,
+#       parameter_3, parameter_4, parameter_5):
+#   function body...
+
+
+# Seperate functions by 2 blank lines
+
+# All import statements should be at the beginning of file
