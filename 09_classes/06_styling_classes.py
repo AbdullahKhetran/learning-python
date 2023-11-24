6750c9b2abbac35ccce2f336f92b2df90c7795bd
Abdullah Khetran <khetranabdullah@gmail.com>
2023-11-24 23:28:29 +0500

    guidelines for styling classes

---
+++ 09_classes/06_styling_classes.py
@@ -0,0 +1,14 @@
+# Classes names should be written in CamelCase
+
+# Instance and modules name in lower_case
+
+# Class docstring should be immediately after the defination
+
+# Module docstring should describe what the classes can be used for
+
+# Blank line
+#   in class: one between method
+#   in modules: two between classes
+
+# Import statements
+# standard library ones first, then blank line, then yours
